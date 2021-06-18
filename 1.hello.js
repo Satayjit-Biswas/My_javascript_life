fff56c6ef2c879552c0f4fd8466d352c26029d80
Satayjit Biswas <61390425+Satayjit-Biswas@users.noreply.github.com>
2021-06-19 02:05:29 +0600

    hellow

---
+++ 1.hello.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+
+
+// I understand that:
+// javascript has 4 type output system
+// for example:
+//     innerhtml
+//     document.write
+//     console.log
+//     alert
+// and it is simple output  
+    
